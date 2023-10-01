17ac7d31fd1baf798511184325cbe6c3a594b807
Reza <RezaTaghipour99@gmail.com>
2023-10-02 02:29:25 +0330

    add blogImage

---
+++ CodePulse.API/Controllers/ImageController.cs
@@ -0,0 +1,67 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImageController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile file, [FromForm] string fileName, [FromForm] string title)
+        {
+            ValidateFileUpload(file);
+            if(ModelState.IsValid)
+            {
+                var blogImage = new BlogImage
+                {
+                    FileExtension = Path.GetExtension(file.FileName).ToLower(),
+                    FileName = fileName,
+                    Title = title,
+                    DateCreated = DateTime.Now
+                };
+
+                blogImage = await imageRepository.Upload(file, blogImage);
+
+                var response = new BlogImageDto()
+                {
+                    DateCreated = blogImage.DateCreated,
+                    FileExtension = blogImage.FileExtension,
+                    FileName = blogImage.FileName,
+                    Id = blogImage.Id,
+                    Title = blogImage.Title,
+                    Url = blogImage.Url
+                };
+
+                return Ok(response);
+
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(IFormFile file)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (!allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower()))
+            {
+                ModelState.AddModelError("file", "Unsupported file format");
+            }
+
+            if(file.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size cannot be more than 10MB");
+            }
+        }
+    }
+}
